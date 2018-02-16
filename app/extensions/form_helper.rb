76c3e738b628df9b760710bd1032cd540e170b19
Tim Hagemann <tim@eternalwings.de>
2018-02-16 18:28:30 +0100

    Fix formhelper to create proper fields
    Previously, using the form helper to create fields for the metadata
    resulted in wrong name values for the html tags and thus the values
    would not end up being saved. This properly sets the name of those tags
    so they can be saved again. In addition, this also adds the date type.

--- app/extensions/form_helper.rb
+++ app/extensions/form_helper.rb
@@ -3,18 +3,24 @@ module ActionView
     module FormTagHelper
       def generate_input_for(video, metadata_info)
         value = video[metadata_info.name]
+        read_only = metadata_info.read_only?
+        options = { readonly: read_only }
+        name = "video[#{metadata_info.name}]" # This is a little ugly ...
 
         case metadata_info.type
           when "number"
-            number_field_tag metadata_info.name, value
+            number_field_tag name, value, options
           when "text"
-            text_field_tag metadata_info.name, value
+            text_field_tag name, value, options
           when "range"
-            range_field_tag metadata_info.name, value, metadata_info.options
+            range_field_tag name, value, options.merge(metadata_info.options)
           when "select"
-            select_tag metadata_info.name, options_for_select(metadata_info.options["values"], value)
+            select_tag name, options_for_select(metadata_info.options[:values], value), options
           when "on_off"
-            check_box_tag metadata_info.name, "1", value
+            check_box_tag name, "1", value, options
+          when "date"
+            str_value = if value then value.strftime("%Y-%m-%d") else "" end
+            date_field_tag name, str_value, options
           else
         end
       end
