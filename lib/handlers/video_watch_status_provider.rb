eb4ace6330ccfbc0675a0b2769c4ea73a2c3ca40
Tim Hagemann <tim@eternalwings.de>
2018-02-18 02:53:10 +0100

    Added some more metadata for watch status provider

--- lib/handlers/video_watch_status_provider.rb
+++ lib/handlers/video_watch_status_provider.rb
@@ -2,14 +2,29 @@ class VideoWatchStatusProvider < MetadataProvider
   def configure(config)
     on_video_load do |video|
       video[:watched] = false
+      video[:watched_times] = 0
+      video[:added_date] = DateTime.now.to_date
     end
 
     on_video_start_play do |video|
       video[:watched] = true
     end
 
+    on_video_finish_play do |video|
+      current_times = video[:watched_times] || 0
+      video[:watched_times] = current_times + 1
+      video[:last_watched] = DateTime.now.to_date
+      if video[:first_watched] == nil
+        video[:first_watched] = DateTime.now.to_date
+      end
+    end
+
     @metadata = [
-      Metadata.new("watched", "on_off")
+      Metadata.new("watched", "on_off"),
+      Metadata.new("watched_times", "number"),
+      Metadata.new("last_watched", "date"),
+      Metadata.new("first_watched", "date", true),
+      Metadata.new("added_date", "date", true)
     ]
   end
 end
