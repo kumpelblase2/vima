85f9a950773d832f973d11b3445839b28d7d9348
Tim Hagemann <tim@eternalwings.de>
2018-06-23 14:02:05 +0200

    Make sure all inputs are porperly styled
    Some inputs, like the date input were not styled like the others. This
    was due to missing the correct css class, which has been fixed.

--- app/extensions/form_helper.rb
+++ app/extensions/form_helper.rb
@@ -4,31 +4,35 @@ module ActionView
       def generate_input_for(video, metadata_info)
         value = video[metadata_info.name]
         read_only = metadata_info.read_only?
-        options = { readonly: read_only }
+        options = {readonly: read_only, class: 'input' }
         name = "video[#{metadata_info.name}]" # This is a little ugly ...
 
         case metadata_info.type
           when "number"
-            number_field_tag name, value, options.merge({class: 'input'})
+            number_field_tag name, value, options
           when "text"
-            text_field_tag name, value, options.merge({ class: 'input' })
+            text_field_tag name, value, options
           when "range"
-            range_field_tag name, value, options.merge({ class: 'range' }).merge(metadata_info.options)
-        when "select"
+            options[:class] = 'range'
+            range_field_tag name, value, options.merge(metadata_info.options)
+          when "select"
             tag.div(class: 'select') do
               select_tag name, options_for_select(metadata_info.options[:values], value), options
             end
           when "on_off"
+            options[:class] = ''
             check_box_tag name, "1", value, options
           when "date"
-            str_value = if value then value.strftime("%Y-%m-%d %H:%M:%S") else "" end
+            str_value = ( value ? value.strftime("%Y-%m-%d %H:%M:%S") : "" )
             datetime_field_tag name, str_value, options
           when "time"
             time_field_tag name, value, options
           when "taglist"
             selected_values = options_from_collection_for_select(MetadataHelper.get_values_for_metadata(metadata_info.name), 'to_s', 'to_s', value)
+            selected_data = { metadata: metadata_info.name, value: value.join(',') }
+            options[:class] = 'taglist'
             tag.div(class: %(select is-multiple)) do
-              select_tag name + "[]", selected_values, options.merge({class: 'taglist', data: { metadata: metadata_info.name, value: value.join(',') }, multiple: 'multiple', style: 'width: 100%'})
+              select_tag name + "[]", selected_values, options.merge({ data: selected_data, multiple: 'multiple', style: 'width: 100%' })
             end
           else
         end
