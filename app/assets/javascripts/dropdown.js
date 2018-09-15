3fee1c47bb7f6a1bd88331064da2b776e8bdc11e
Tim Hagemann <tim@eternalwings.de>
2018-09-15 18:05:34 +0200

    Hide dropdown after clicking somewhere else other than its menu

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -2,4 +2,10 @@ $(document).ready(() => {
     $('.dropdown-trigger').click(event => {
         $(event.currentTarget.parentNode).toggleClass('is-active');
     });
+
+    $(document).click(event => {
+        if(!event.target.parentElement.className.includes("dropdown-trigger")) {
+            $('.dropdown').removeClass('is-active');
+        }
+    });
 });
