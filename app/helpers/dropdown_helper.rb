ff37f134e4ed81992294f07bbe1d7feda835d0ae
Tim Hagemann <tim@eternalwings.de>
2018-09-15 17:27:40 +0200

    Order created_at and updated_at descending by default

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -1,10 +1,13 @@
 module DropdownHelper
-  DEFAULT_ORDERING = 'asc'
+  DEFAULT_ORDERING = Hash.new {|hash, key| hash[key] = 'asc'}
+
+  DEFAULT_ORDERING['created_at'] = 'desc'
+  DEFAULT_ORDERING['updated_at'] = 'desc'
 
   def video_ordering_dropdown_item(display_name, attribute)
     selected = params[:order] == attribute
     current_ordering = params[:dir]
-    ordering_attributes = VideoHelper.ordering_options(attribute, selected, DEFAULT_ORDERING, current_ordering)
+    ordering_attributes = VideoHelper.ordering_options(attribute, selected, DEFAULT_ORDERING[attribute], current_ordering)
     query_attributes = ordering_attributes.merge({search: params[:search]})
     render_parameters = {
         query_params: query_attributes,
