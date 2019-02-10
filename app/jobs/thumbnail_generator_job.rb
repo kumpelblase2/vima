aa624a7581eea702d7e3025659cf3bc6aeeb6814
Tim Hagemann <tim@eternalwings.de>
2019-02-10 11:23:20 +0100

    Always generate thumbnails
    Thumbnails have been a core part of the application and in many cases
    its assumed that at least one thumbnail exists. Therefor the setting
    would only break things.

--- app/jobs/thumbnail_generator_job.rb
+++ app/jobs/thumbnail_generator_job.rb
@@ -4,20 +4,18 @@ class ThumbnailGeneratorJob < ApplicationJob
   def perform(*args)
     config = Rails.configuration
     thumbnail_config = config.library["thumbnails"]
-    if thumbnail_config["generate"]
-      thumb_dir = File.join(config.library["dir"], thumbnail_config["dir"])
-      Dir.mkdir thumb_dir unless File.exists? thumb_dir
+    thumb_dir = File.join(config.library["dir"], thumbnail_config["dir"])
+    Dir.mkdir thumb_dir unless File.exists? thumb_dir
 
-      amount = thumbnail_config["amount"]
-      args.each do |video_loc|
-        file_hash = FileHelper.get_file_hash(video_loc)
-        video = Video.find_by_hash file_hash
-        if video
-          puts "Generating #{amount} thumbnails for #{video_loc}"
-          thumbnails = ImageHelper.generate_thumbnails video_loc, amount, File.join(thumb_dir, file_hash + "_%d.jpg")
-          thumbnails = thumbnails.map { |file| File.basename(file) }
-          video.update(selected_thumbnail: 0, thumbnails: thumbnails)
-        end
+    amount = thumbnail_config["amount"] || 1
+    args.each do |video_loc|
+      file_hash = FileHelper.get_file_hash(video_loc)
+      video = Video.find_by_hash file_hash
+      if video
+        puts "Generating #{amount} thumbnails for #{video_loc}"
+        thumbnails = ImageHelper.generate_thumbnails video_loc, amount, File.join(thumb_dir, file_hash + "_%d.jpg")
+        thumbnails = thumbnails.map { |file| File.basename(file) }
+        video.update(selected_thumbnail: 0, thumbnails: thumbnails)
       end
     end
   end
