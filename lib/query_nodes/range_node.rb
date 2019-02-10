94d2550fa2f1c1c49fd24555d7061ceb3efa5149
Tim Hagemann <tim@eternalwings.de>
2019-02-10 14:57:32 +0100

    Allow for querying the size of a taglist using `<` & `>`

--- lib/query_nodes/range_node.rb
+++ lib/query_nodes/range_node.rb
@@ -7,7 +7,16 @@ module QueryNodes
     end
 
     def apply(query, keys, is_and = false)
-      query.where((@smaller ? @key.lt : @key.gt) => @value)
+      metadata = keys[@key]
+      if metadata.type == "taglist"
+        if @smaller
+          query.where(:"#{@key}.#{@value - 1}".exists => false)
+        else
+          query.where(:"#{@key}.#{@value}".exists => true)
+        end
+      else
+        query.where((@smaller ? @key.lt : @key.gt) => @value)
+      end
     end
 
     def is_applicable?(keys)
