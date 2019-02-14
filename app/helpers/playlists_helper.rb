15e0013fcd39c5f13e990992ad043387b535ec64
Tim Hagemann <tim@eternalwings.de>
2019-02-14 17:57:49 +0100

    Make sure we use the ordered videos to calculate next video

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,7 +1,7 @@
 module PlaylistsHelper
   def self.get_video_after(playlist, current)
     return nil unless playlist
-    videos = playlist.videos
+    videos = playlist.ordered_videos
     current_index = videos.index {|video| video._id == current._id}
     next_index = current_index + 1
     if next_index < videos.size
